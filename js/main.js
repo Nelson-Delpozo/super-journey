e47eb27343f8dcecb7cf9501e995aceee521548d
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-01-21 12:30:27 -0600

    katas

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
 "use strict";
 console.log("connected");
+
+// FUNCTION TO TURN NUMBER TO STRING
+
+function numToString(num){
+    return num.toString();
+}
+
+console.log(numToString(9));
+console.log(typeof (numToString(9)));
+
+
+// FUNCTION TO RETURN STRING BASED ON BOOLEAN
+
+function boolToWord( bool ){
+    if(bool){
+        return "Yes"
+    }else return "No"
+}
+
+console.log(boolToWord(true));
+console.log(boolToWord(false));
+
+// FUNCTION TO ADD NUMBERS BETWEEN TWO NUMBERS
+
