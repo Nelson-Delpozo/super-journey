cdf7a9db521bc988aca99327314e566e4db447aa
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-06-30 21:00:32 -0500

    codewars

--- js/main.js
+++ js/main.js
@@ -57,3 +57,20 @@ function repeatStr (n, s) {
         }
         return newStr;
 }
+
+// Write a function which calculates the average of the numbers in a given list.
+//
+//     Note: Empty arrays should return 0.
+
+function find_average(array) {
+    // your code here
+    let total = 0;
+    if(array.length === 0){
+        return 0;
+    }else{
+    for(let i = 0; i < array.length; i++){
+        total += array[i];
+    }
+    return total / array.length;
+}
+    }
