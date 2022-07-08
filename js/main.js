7448f125a1e9ebc63ef0951611802ce648896094
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-07-08 15:24:34 -0500

    challenges

--- js/main.js
+++ js/main.js
@@ -146,7 +146,7 @@ function People () {
 // Define "athlete" property on the People prototype so that "athlete" is accessible by all objects that use the People () constructor.
 People.prototype.athlete = "Tiger Woods";
 
-var famousPerson = new People ();
+let famousPerson = new People ();
 famousPerson.superstar = "Steve Jobs";
 
 // The search for superstar will first look for the superstar property on the famousPerson object, and since we defined it there, that is the property that will be used. Because we have overwritten the famousPerson’s superstar property with one directly on the famousPerson object, the search will NOT proceed up the prototype chain.
@@ -159,3 +159,22 @@ console.log (famousPerson.athlete); // Tiger Woods
 
 // In this example, the search proceeds up the prototype chain and find the toString method on Object.prototype, from which the Fruit object inherited—all objects ultimately inherits from Object.prototype as we have noted before.
 console.log (famousPerson.toString()); // [object Object]
+
+let animal = {
+    jumps: null
+};
+let rabbit = {
+    __proto__: animal,
+    jumps: true
+};
+
+alert( rabbit.jumps ); // ? (1)
+//true
+delete rabbit.jumps;
+
+alert( rabbit.jumps ); // ? (2)
+//null
+delete animal.jumps;
+
+alert( rabbit.jumps ); // ? (3)
+//undefined
